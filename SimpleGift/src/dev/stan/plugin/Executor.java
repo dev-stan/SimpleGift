c50d87e10f6b4ff179a15c3f71332cd7d72b46dc
dev-stan <mrstanleyzalewski@gmail.com>
2021-03-09 14:58:46 +0100

    Update Executor

--- SimpleGift/src/dev/stan/plugin/Executor.java
+++ SimpleGift/src/dev/stan/plugin/Executor.java
@@ -1,5 +1,29 @@
 package dev.stan.plugin;
 
-public class Executor {
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Executor extends JavaPlugin implements Listener {
+
+	public static String prefix = ChatColor.DARK_AQUA + "[" + ChatColor.GREEN + "Mention" + ChatColor.DARK_AQUA + "] ";
+	public static String noperms = Executor.prefix + ChatColor.RED			+ "You do not have permission to perform this command.";
+	public static Boolean update = (Boolean) false;
+
+	// On enable method, execute as soon as plugin is enabled.
+	@Override
+	public void onEnable() {
+		
+		// getLogger() instead of sysout on enable
+		getLogger().info("Mention Plugin enabled.");
+
+		// Create new instance of Commands class and point to executor
+		getCommand("ping").setExecutor(new PingToggleCMD());
+
+		// Create new instance of Events class and point to executor
+		getServer().getPluginManager().registerEvents(new ChatEventPing(), this);
+
+	}
 
 }
