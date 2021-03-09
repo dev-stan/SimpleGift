5b221a034cbc30185772c1612f3c4da612ae9a63
dev-stan <mrstanleyzalewski@gmail.com>
2021-03-09 15:09:25 +0100

    Added plugin.yml

--- SimpleGift/src/dev/stan/plugin/Executor.java
+++ SimpleGift/src/dev/stan/plugin/Executor.java
@@ -1,6 +1,5 @@
 package dev.stan.plugin;
 
-import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,7 +20,7 @@ public class Executor extends JavaPlugin implements Listener {
 		getCommand("ping").setExecutor(new GiftCMD());
 
 		// Create new instance of Events class and point to executor
-		// getServer().getPluginManager().registerEvents(new ChatEventPing(), this);
+		// getServer().getPluginManager().registerEvents(new ChatEvent(), this);
 
 	}
 
--- SimpleGift/src/dev/stan/plugin/commands/GiftCMD.java
+++ SimpleGift/src/dev/stan/plugin/commands/GiftCMD.java
@@ -1,14 +1,25 @@
 package dev.stan.plugin.commands;
 
+import java.util.HashMap;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class GiftCMD implements CommandExecutor {
+	
+	public HashMap<String, Long> cooldowns = new HashMap<String, Long>();
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if (cmd.getName().equalsIgnoreCase("gift")) {
+			
+			Player player = (Player) sender;
+			
+			
+		}
 		return false;
 	}
 
