8bd97050a6d1294e7ef5ceae87bef592395ffe33
dev-stan <mrstanleyzalewski@gmail.com>
2021-03-09 21:23:41 +0100

    Update Executor class

--- SimpleGift/src/dev/stan/plugin/events/HelpChatEvent.java
+++ SimpleGift/src/dev/stan/plugin/events/HelpChatEvent.java
@@ -1,20 +1,5 @@
 package dev.stan.plugin.events;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+public class HelpChatEvent {
 
-public class HelpChatEvent extends JavaPlugin implements Listener{
-	
-	@EventHandler
-	public void onChat(AsyncPlayerChatEvent event) {
-		
-		Player player = event.getPlayer();	
-		
-		if (event.getMessage().contains("how") || event.getMessage().contains("gift")) {
-			player.sendMessage("{stuff}");
-		}
-	}
 }
