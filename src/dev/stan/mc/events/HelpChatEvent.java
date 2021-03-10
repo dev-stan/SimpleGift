c15616068db7f76f53a5edceb5ea7670aeb26368
dev-stan <mrstanleyzalewski@gmail.com>
2021-03-10 21:14:22 +0100

    Added onChat help messages

--- src/dev/stan/mc/events/HelpChatEvent.java
+++ src/dev/stan/mc/events/HelpChatEvent.java
@@ -1,5 +1,25 @@
 package dev.stan.mc.events;
 
-public class HelpChatEvent {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
+public class HelpChatEvent extends JavaPlugin implements Listener{
+
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		
+		Player player = event.getPlayer();
+		
+		if (event.getMessage().contains("how to gift")) {
+			
+			player.sendMessage(ChatColor.GOLD + "Use the /gift command in order to give an item to a player.");
+		}
+		
+		
+	}
 }
