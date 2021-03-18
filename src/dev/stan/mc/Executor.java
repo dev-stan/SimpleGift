cd02c68ff7aa00c3b0933447cebff1d7545e5602
dev-stan <mrstanleyzalewski@gmail.com>
2021-03-18 01:12:24 +0100

    Updated config defaults

--- src/dev/stan/mc/Executor.java
+++ src/dev/stan/mc/Executor.java
@@ -1,22 +1,30 @@
 package dev.stan.mc;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.stan.mc.commands.GiftCMD;
-import dev.stan.mc.events.ConfirmListenerEvent;
 import dev.stan.mc.events.HelpChatEvent;
 
 public class Executor extends JavaPlugin implements Listener {
 
-
+	FileConfiguration config = this.getConfig();
+	
+	
 	// On enable method, execute as soon as plugin is enabled.
 	@Override
 	public void onEnable() {
 		
+		// Writing to the config
+		config.addDefault("messages.general.prefix", "&6[&eGIFT&6] ");
+		config.addDefault("messages.general.success", "&aGift sent succesfully!");
 		
-		// Create and save plugin config
-		this.getConfig().options().copyDefaults();
+		config.addDefault("messages.error.no-permission", "&4You do not have permission to perform this command!");
+		config.addDefault("messages.error.inventory-full", "&4Player inventory is full!");
+		
+		
+		this.getConfig().options().copyDefaults(); // Copy config defaults
 		saveDefaultConfig();
 
 		// Create new instance of Commands class and point to executor
